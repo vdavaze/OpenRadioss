d8e84742765aedf4444ca0095cda797b94a61f2e
Valentin Davaze <103204878+vdavaze@users.noreply.github.com>
2026-05-05 16:57:57 +0200

    End of day 05052026

--- engine/source/materials/mat/mat136/sigeps136g.F90
+++ engine/source/materials/mat/mat136/sigeps136g.F90
@@ -23,15 +23,14 @@
       module sigeps136g_mod
       contains
         subroutine sigeps136g(                                                 &
-          nel    ,matparam,rho    ,dmg     ,                                   &
+          nel    ,matparam,rho    ,dmg     ,thk0   ,                           &
           depsxx ,depsyy ,depsxy  ,depsyz  ,depszx ,                           &                             
           depbxx ,depbyy ,depbxy  ,                                            &
           sigoxx ,sigoyy ,sigoxy  ,sigoyz  ,sigozx ,                           &
-          momoxx ,momoyy ,momoxy  ,                                            &
           signxx ,signyy ,signxy  ,signyz  ,signzx ,                           &
           momnxx ,momnyy ,momnxy  ,                                            &
-          ssp    ,et     ,gs      )
-
+          ssp    ,et     ,gs      ,nuvar   ,uvar   ,                           &
+          shf    )
 !----------------------------------------------------------------
 !   M o d u l e s
 !----------------------------------------------------------------
@@ -48,7 +47,8 @@
           integer,                       intent(in)    :: nel      !< Number of elements in the group
           type(matparam_struct_),        intent(in)    :: matparam !< Material parameters data
           real(kind=WP), dimension(nel), intent(in)    :: rho      !< Density at current time
-          real(kind=WP), dimension(nel,2),intent(inout):: dmg      !< Bending damage at current time
+          real(kind=WP), dimension(nel,3),intent(inout):: dmg      !< Bending damage at current time
+          real(kind=WP), dimension(nel), intent(in)    :: thk0     !< Initial thickness
           real(kind=WP), dimension(nel), intent(in)    :: depsxx   !< Membrane strain increment xx
           real(kind=WP), dimension(nel), intent(in)    :: depsyy   !< Membrane strain increment yy
           real(kind=WP), dimension(nel), intent(in)    :: depsxy   !< Membrane strain increment xy
@@ -62,9 +62,6 @@
           real(kind=WP), dimension(nel), intent(in)    :: sigoxy   !< Membrane force xy at previous time step
           real(kind=WP), dimension(nel), intent(in)    :: sigoyz   !< Membrane force yz at previous time step
           real(kind=WP), dimension(nel), intent(in)    :: sigozx   !< Membrane force zx at previous time step
-          real(kind=WP), dimension(nel), intent(in)    :: momoxx   !< Bending moment xx at previous time step
-          real(kind=WP), dimension(nel), intent(in)    :: momoyy   !< Bending moment yy at previous time step
-          real(kind=WP), dimension(nel), intent(in)    :: momoxy   !< Bending moment xy at previous time step
           real(kind=WP), dimension(nel), intent(inout) :: signxx   !< Membrane force xx at current time step
           real(kind=WP), dimension(nel), intent(inout) :: signyy   !< Membrane force yy at current time step
           real(kind=WP), dimension(nel), intent(inout) :: signxy   !< Membrane force xy at current time step
@@ -75,15 +72,22 @@
           real(kind=WP), dimension(nel), intent(inout) :: momnxy   !< Bending moment xy at current time
           real(kind=WP), dimension(nel), intent(inout) :: ssp      !< Current sound speed
           real(kind=WP), dimension(nel), intent(inout) :: et       !< Hourglass stabilization variable
-          real(kind=WP), dimension(nel), intent(in)    :: gs       !< Correction factor for transverse shear
+          real(kind=WP), dimension(nel), intent(inout) :: gs       !< Correction factor for transverse shear
+          integer,                       intent(in)    :: nuvar    !< Number of user variables
+          real(kind=WP), dimension(nel,nuvar), intent(inout) :: uvar !< User variables at current time step
+          real(kind=WP), dimension(nel), intent(in)    :: shf      !< Shear correction factor force coefficient
 !----------------------------------------------------------------
 !  L o c a l  V a r i a b l e s
 !----------------------------------------------------------------
           integer :: i
-          real(kind=WP) :: young,nu,lambda_m, mu_m, lambda_b, mu_b, mu_tsh
-          real(kind=WP) :: k0_1, k0_2, dmax1, dmax2
+          real(kind=WP) :: young,nu,shear,lambda_m, mu_m, gamma
+          real(kind=wp), dimension(nel) :: lambda_b, mu_b, k0_1, k0_2
+          real(kind=WP) :: dmax, f_t, f_c, center, radius, kappa1, kappa2
           real(kind=WP) :: tr_kb, phi_b, Y1, Y2, d1_trial, d2_trial
-          real(kind=WP), dimension(nel) :: xi_tr, xi_xy
+          real(kind=WP), dimension(nel) :: xi_tr, epbxx, epbyy, epbxy, xi_kap1,&
+            xi_kap2
+          real(kind=WP) :: dkap1dkxx, dkap1dkyy, dkap1dkxy, dkap2dkxx,         &
+            dkap2dkyy, dkap2dkxy, phi_b_pos, phi_b_neg
 !
           !=====================================================================
           !< - Initialisation of computation on time step
@@ -92,15 +96,28 @@
           !< Recovering real model parameters
           young    = matparam%young
           nu       = matparam%nu
-          lambda_m = matparam%uparam(2)
-          mu_m     = matparam%uparam(3)
-          lambda_b = matparam%uparam(4)
-          mu_b     = matparam%uparam(5)
-          mu_tsh   = matparam%uparam(6)
-          k0_1     = matparam%uparam(7)
-          k0_2     = matparam%uparam(8)
-          dmax1    = matparam%uparam(9)
-          dmax2    = matparam%uparam(10)
+          shear    = matparam%shear
+          lambda_m = matparam%uparam(1)
+          mu_m     = matparam%uparam(2)
+          lambda_b(1:nel) = thk0(1:nel)*matparam%uparam(3)
+          mu_b(1:nel) = thk0(1:nel)*matparam%uparam(4)
+          gs(1:nel) = shear*shf(1:nel)
+          f_t      = matparam%uparam(5)
+          f_c      = matparam%uparam(6)
+          k0_1(1:nel) = f_t*f_t*(one - nu*nu) / (six*young*thk0(1:nel)*thk0(1:nel))
+          k0_2(1:nel) = f_c*f_c*(one - nu*nu) / (six*young*thk0(1:nel)*thk0(1:nel))
+          gamma    = matparam%uparam(7)
+          dmax     = matparam%uparam(8)
+!
+          !< Recover the damage variable at the beginning of the time step
+          do i = 1, nel
+            uvar(i,1) = uvar(i,1) + depbxx(i)
+            uvar(i,2) = uvar(i,2) + depbyy(i)
+            uvar(i,3) = uvar(i,3) + depbxy(i)
+            epbxx(i)  = uvar(i,1)
+            epbyy(i)  = uvar(i,2)
+            epbxy(i)  = uvar(i,3)
+          enddo
 !
           !=====================================================================
           !< - Computation of the trial stress tensor (elastic predictor)
@@ -113,19 +130,23 @@
             signyy(i) = sigoyy(i) + lambda_m*(depsxx(i) + depsyy(i)) +         &
                                                   two*mu_m*depsyy(i)
             signxy(i) = sigoxy(i) +   mu_m*depsxy(i)
-            signyz(i) = sigoyz(i) + mu_tsh*depsyz(i)*gs(i)
-            signzx(i) = sigozx(i) + mu_tsh*depszx(i)*gs(i)
-            ! -> Bending moments
-            momnxx(i) = momoxx(i) + lambda_b*(depbxx(i) + depbyy(i)) +         &
-                                                  two*mu_b*depbxx(i)
-            momnyy(i) = momoyy(i) + lambda_b*(depbxx(i) + depbyy(i)) +         &
-                                                  two*mu_b*depbyy(i)
-            momnxy(i) = momoxy(i) + mu_b*depbxy(i)
+            signyz(i) = sigoyz(i) +  gs(i)*depsyz(i)
+            signzx(i) = sigozx(i) +  gs(i)*depszx(i)
             ! -> Hourglass stabilization variable
             et(i) = one
             ! -> Sound speed
             ssp(i) = sqrt((young/(one - nu*nu))/rho(i))
           enddo
+!
+          ! !=====================================================================
+          ! !< - Computation of the plasticity
+          ! !=====================================================================
+          ! ! !< Loop over elements
+          ! ! do i = 1, nel
+          !  Mf_pos = (thk0(i)/four)*f_c*(one - (signxx(i)/(f_c*thk0(i)))**2) - &
+          !           Om_sup*sigy*(thk0(i)/two - dsup) -                        &
+          !           Om_inf*sigy*(thk0(i)/two - dinf)
+          ! ! enddo
 !
           !=====================================================================
           !< - Computation of the bending damage and softening factor
@@ -133,45 +154,78 @@
           !< Loop over elements
           do i = 1, nel
             !< Trace of the bending curvature increment tensor
-            tr_kb = depbxx(i) + depbyy(i)
-            !< Bending damage energy release rate
-            phi_b = half*lambda_b * tr_kb*tr_kb                                &
-              + mu_b * (depbxx(i)*depbxx(i) + depbyy(i)*depbyy(i)              &
-              + half*half*depbxy(i)*depbxy(i))
-            !< Thermodynamic force conjugate to the bending damages
-            Y1 = phi_b / ((one + dmg(i,1)) * (one + dmg(i,1)))
-            Y2 = phi_b / ((one + dmg(i,2)) * (one + dmg(i,2)))
+            tr_kb = epbxx(i) + epbyy(i)
+            !< Eigenvalues of the bending curvature tensor
+            center = (epbxx(i) + epbyy(i))/two
+            radius = sqrt(((epbxx(i) - epbyy(i))*half)**2 + (epbxy(i)*half)**2)
+            kappa1 = center + radius
+            kappa2 = center - radius
+            !< Free elastic bending energy (without damage)
+            phi_b = half*lambda_b(i) * tr_kb*tr_kb                             &
+                           + mu_b(i) * ( kappa1*kappa1  + kappa2*kappa2 )
+            !< Thermodynamic force Y1 (positive bending - tension inner face)
+            Y1 = zero
+            if (tr_kb  >= zero) Y1 = Y1 + half*lambda_b(i) * tr_kb*tr_kb
+            if (kappa1 >= zero) Y1 = Y1 + mu_b(i) * kappa1*kappa1
+            if (kappa2 >= zero) Y1 = Y1 + mu_b(i) * kappa2*kappa2
+            phi_b_pos = Y1
+            Y1 = ((one - gamma)/(one + dmg(i,2))**2) * Y1
+            !< Thermodynamic force Y2 (negative bending - tension outer face)
+            Y2 = zero
+            if (tr_kb  < zero) Y2 = Y2 + half*lambda_b(i) * tr_kb*tr_kb
+            if (kappa1 < zero) Y2 = Y2 + mu_b(i) * kappa1*kappa1
+            if (kappa2 < zero) Y2 = Y2 + mu_b(i) * kappa2*kappa2
+            phi_b_neg = Y2
+            Y2 = ((one - gamma)/(one + dmg(i,3))**2) * Y2
             !< Evolution law of the bending damage
             ! --> Positive bending, inner face
-            if (tr_kb > zero) then 
-              if (Y1 > k0_1) then
-                d1_trial = sqrt(phi_b / k0_1) - one
-                d1_trial = max(zero, min(d1_trial, dmax1))
-                dmg(i,1) = max(dmg(i,1), d1_trial)
-              end if
+            if (Y1 >= k0_1(i)) then
+              d1_trial = sqrt(phi_b_pos / k0_1(i)) - one
+              d1_trial = max(zero, min(d1_trial, dmax))
+              dmg(i,2) = max(dmg(i,2), d1_trial)
+            end if
             ! --> Negative bending, outer face
-            else 
-              if (Y2 > k0_2) then
-                d2_trial = sqrt(phi_b / k0_2) - one
-                d2_trial = max(zero, min(d2_trial, dmax2))
-                dmg(i,2) = max(dmg(i,2), d2_trial)
-              end if
-            endif
+            if (Y2 >= k0_2(i)) then
+              d2_trial = sqrt(phi_b_neg / k0_2(i)) - one
+              d2_trial = max(zero, min(d2_trial, dmax))
+              dmg(i,3) = max(dmg(i,3), d2_trial)
+            end if
             !< Softening factor for the moment Mxx and Myy
             if (tr_kb >= zero) then
-              xi_tr(i) = one / (one + dmg(i,1))
+              xi_tr(i) = (one + gamma*dmg(i,2)) / (one + dmg(i,2))
             else
-              xi_tr(i) = one / (one + dmg(i,2))
+              xi_tr(i) = (one + gamma*dmg(i,3)) / (one + dmg(i,3))
             end if
-            !< Softening factor for the moment Mxy
-            if (depbxy(i) >= zero) then
-              xi_xy(i) = one / (one + dmg(i,1))
+            !< Softening factor for kappa1 and kappa2
+            if (kappa1 >= zero) then
+              xi_kap1(i) = (one + gamma*dmg(i,2)) / (one + dmg(i,2))
             else
-              xi_xy(i) = one / (one + dmg(i,2))
+              xi_kap1(i) = (one + gamma*dmg(i,3)) / (one + dmg(i,3))
             endif
-
-          enddo
+            if (kappa2 >= zero) then
+              xi_kap2(i) = (one + gamma*dmg(i,2)) / (one + dmg(i,2))
+            else
+              xi_kap2(i) = (one + gamma*dmg(i,3)) / (one + dmg(i,3))
+            endif
+            dmg(i,1) = max(dmg(i,2), dmg(i,3)) !< Maximum of the positive and negative bending damage
+            !< Update the bending moments with the softening factors
+            dkap1dkxx = half + (epbxx(i) - epbyy(i))/(four*max(radius,em20))
+            dkap1dkyy = half - (epbxx(i) - epbyy(i))/(four*max(radius,em20))
+            dkap1dkxy = (epbxy(i)*half)/(two*max(radius,em20))
+            dkap2dkxx = half - (epbxx(i) - epbyy(i))/(four*max(radius,em20))
+            dkap2dkyy = half + (epbxx(i) - epbyy(i))/(four*max(radius,em20))
+            dkap2dkxy = -(epbxy(i)*half)/(two*max(radius,em20))
+            momnxx(i) = lambda_b(i)*tr_kb*xi_tr(i) + two*mu_b(i)*(             &
+                            xi_kap1(i)*kappa1*dkap1dkxx +                      &
+                            xi_kap2(i)*kappa2*dkap2dkxx ) 
+            momnyy(i) = lambda_b(i)*tr_kb*xi_tr(i) + two*mu_b(i)*(             & 
+                            xi_kap1(i)*kappa1*dkap1dkyy +                      &
+                            xi_kap2(i)*kappa2*dkap2dkyy )
+            momnxy(i) = two*mu_b(i)*(                                          &
+                            xi_kap1(i)*kappa1*dkap1dkxy +                      &
+                            xi_kap2(i)*kappa2*dkap2dkxy )
+          enddo       
 !
-          !=========================================================================
+          !=====================================================================
         end subroutine sigeps136g
       end module sigeps136g_mod
--- starter/source/materials/mat/mat136/hm_read_mat136.F90
+++ starter/source/materials/mat/mat136/hm_read_mat136.F90
@@ -22,8 +22,8 @@
 !Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
       module hm_read_mat136_mod
       contains
-        subroutine hm_read_mat136(                                               &
-          matparam ,nuvar    ,nfunc    ,parmat   ,unitab   ,mat_id   ,titr     , &
+        subroutine hm_read_mat136(                                             &
+          matparam ,nuvar    ,nfunc    ,parmat   ,unitab   ,mat_id   ,titr   , &
           mtag     ,nvartmp  ,lsubmodel,ntable   ,table    ,iout     )
 !-----------------------------------------------
 !   M o d u l e s
@@ -61,8 +61,8 @@
 !-----------------------------------------------
 !   L o c a l   V a r i a b l e s
 !-----------------------------------------------
-          real(kind=WP) :: e, nu, rho0, bulk, shear, hthick, lambda_m, mu_m,   &
-            lambda_b, mu_b, tshear, f_c, f_t, kfiss1, kfiss2
+          real(kind=WP) :: e, nu, rho0, bulk, shear, lambda_m, mu_m, lambda_b, &
+             mu_b, tshear, f_c, f_t, kfiss1, kfiss2, dmax, qp1, qp2, gamma
           integer :: ilaw
           logical :: is_available,is_encrypted
 !-----------------------------------------------
@@ -70,7 +70,7 @@
 !-----------------------------------------------
           is_encrypted = .false.
           is_available = .false.
-          ilaw= 136
+          ilaw = 136
           !----------------------------------------------------------------------------------
           call hm_option_is_encrypted(is_encrypted)
           !----------------------------------------------------------------------------------
@@ -80,9 +80,13 @@
           !< 1st line of material card
           call hm_get_floatv('MAT_E'            ,e      ,is_available, lsubmodel, unitab)
           call hm_get_floatv('MAT_NU'           ,nu     ,is_available, lsubmodel, unitab)
-          call hm_get_floatv('MAT_H'            ,hthick ,is_available, lsubmodel, unitab)
           call hm_get_floatv('MAT_FT'           ,f_t    ,is_available, lsubmodel, unitab)
           call hm_get_floatv('MAT_FC'           ,f_c    ,is_available, lsubmodel, unitab)
+          call hm_get_floatv('MAT_GAMMA'        ,gamma  ,is_available, lsubmodel, unitab)
+          !----------------------------------------------------------------------------------
+          !< 2nd line of material card
+          call hm_get_floatv('MAT_QP1'          ,qp1    ,is_available, lsubmodel, unitab)
+          call hm_get_floatv('MAT_QP2'          ,qp2    ,is_available, lsubmodel, unitab)
           !----------------------------------------------------------------------------------
 !
           !----------------------------------------------------------------------------------
@@ -93,23 +97,20 @@
           !< Elastic constants
           !----------------------------------------------------------------------------------
           !< Membrane elastic parameters
-          lambda_m = e*nu*hthick/(one - nu*nu)
-          mu_m     = e*hthick/(two*(one + nu))
+          lambda_m = e*nu/(one - nu*nu)
+          mu_m     = e/(two*(one + nu))
           !< Bending elastic parameters
-          lambda_b = e*nu*hthick**3/(12.0d0*(one - nu*nu))
-          mu_b     = e*hthick**3/(24.0d0*(one + nu))
+          lambda_b = e*nu/(12.0d0*(one - nu*nu))
+          mu_b     = e/(24.0d0*(one + nu))
           !< Shear modulus
-          shear = e/(two*(one+nu))
+          shear    = e/(two*(one + nu))
           !< Bulk modulus
-          bulk  = e/(three*(one - two*nu))
-          !< Transverse shear coefficient
-          tshear = hthick*e/(two*(one + nu))
+          bulk     = e/(three*(one - two*nu))
 !
           !----------------------------------------------------------------------------------
           !< Damage constants
           !----------------------------------------------------------------------------------
-          kfiss1 = six*f_t/(e*hthick*hthick)
-          kfiss2 = six*f_c/(e*hthick*hthick)   
+          dmax = (one - qp2/qp1)/(qp2/qp1 - gamma)
 !
           !----------------------------------------------------------------------------------
           !< Filling buffer tables
@@ -117,9 +118,9 @@
           !< Number of integer material parameters
           matparam%niparam = 1
           !< Number of real material parameters
-          matparam%nuparam = 6
+          matparam%nuparam = 8
           !< Number of user variables
-          nuvar = 0
+          nuvar = 5
           !< Number of functions
           nfunc = 0
           !< Number of tables and temporary variables
@@ -140,17 +141,21 @@
           matparam%nu        = nu
           matparam%shear     = shear
           matparam%bulk      = bulk
-          matparam%uparam(1) = hthick
-          matparam%uparam(2) = lambda_m
-          matparam%uparam(3) = mu_m
-          matparam%uparam(4) = lambda_b
-          matparam%uparam(5) = mu_b
-          matparam%uparam(6) = tshear
+          matparam%uparam(1) = lambda_m
+          matparam%uparam(2) = mu_m
+          matparam%uparam(3) = lambda_b
+          matparam%uparam(4) = mu_b
+          matparam%uparam(5) = f_t
+          matparam%uparam(6) = f_c
+          matparam%uparam(7) = gamma
+          matparam%uparam(8) = dmax
 !
           !< PARMAT table
           parmat(1)  = bulk
           parmat(2)  = e
           parmat(3)  = nu
+          parmat(16) = 2 
+          parmat(17) = two*shear/(bulk+four_over_3*shear)
 !
           !< Initial and reference density
           matparam%rho0 = rho0
@@ -161,8 +166,14 @@
           mtag%l_epsd = 1
           mtag%g_pla  = 1
           mtag%l_pla  = 1
-          mtag%g_dmg  = 2
-          mtag%l_dmg  = 2
+          mtag%g_dmg  = 3
+          mtag%l_dmg  = 3
+!
+          ! Number of output modes 
+          matparam%nmod = 2
+          allocate(matparam%mode(matparam%nmod))
+          matparam%mode(1) = "Positive bending damage"
+          matparam%mode(2) = "Negative bending damage"
 !
           !< Properties compatibility
           call init_mat_keyword(matparam,"SHELL_ISOTROPIC")
@@ -182,7 +193,7 @@
             write(iout,'(5X,A,//)') 'CONFIDENTIAL DATA'
           else
             write(iout,1002) rho0
-            write(iout,1003) e,nu,hthick
+            write(iout,1003) e,nu,f_t,f_c,gamma,qp1,qp2,dmax
           endif
 !
           !----------------------------------------------------------------------------------
@@ -203,7 +214,12 @@
             5X,"-------------------                                     ",/,       &
             5X,"YOUNG MODULUS (E) . . . . . . . . . . . . . . . . . . .=",1PG20.13/&
             5X,"POISSON RATIO (NU). . . . . . . . . . . . . . . . . . .=",1PG20.13/&
-            5X,"CONCRETE THICKNESS (H). . . . . . . . . . . . . . . . .=",1PG20.13/)
+            5X,"TENSILE STRENGTH (FT) . . . . . . . . . . . . . . . . .=",1PG20.13/&
+            5X,"COMPRESSIVE STRENGTH (FC) . . . . . . . . . . . . . . .=",1PG20.13/&
+            5X,"GAMMA . . . . . . . . . . . . . . . . . . . . . . . . .=",1PG20.13/&
+            5X,"SLOPE 1 . . . . . . . . . . . . . . . . . . . . . . . .=",1PG20.13/&
+            5X,"SLOPE 2 . . . . . . . . . . . . . . . . . . . . . . . .=",1PG20.13/&
+            5X,"MAXIMUM DAMAGE (DMAX) . . . . . . . . . . . . . . . . .=",1PG20.13/)
 !
         end subroutine hm_read_mat136
       end module hm_read_mat136_mod
